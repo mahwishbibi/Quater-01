a510f7d62c6f9cda18d641cac44f5d43372fa0d5
Dr.Mehwish <166476024+mahwishbibi@users.noreply.github.com>
2025-02-01 17:51:08 +0500

    Add files via upload

---
+++ Practice-02/main.js
@@ -0,0 +1,19 @@
+// console.log ("Naeem CodeHub");  comment clt+?
+// 1. Variable
+// 2. word vs keyword
+// 3. data type 
+// 1. Variable... 
+// code kay ander kisi data ko store krny k leye hm variable ka use krty hain 
+// let x1 ="Mehwish Hussain"
+// console.log(x1);
+// 2. Keyword... koi be word jis ka TS m koi matalb ho this is called keyword like Let
+// 2. Word... koi b esa word jis ka TS m koi mtlb na ho like boy girl women (word)
+// let x1 = "Mehwish Hussain"
+// 3.Data Type... koi b esa data jo text m ho us ko hm string kety hain 
+// let x1 = "Mehwish Hussain" x1 m error ay ga qk us ko string show krna hai hmy ts m 
+// let x : string = "Mehwish Hussain";
+var myName = "Mehwish Hussain";
+// Number... jo b data number m ho number type kehlata hai
+var age = 25;
+// console.log(myName);
+console.log(age);
---
+++ Practice-02/main.ts
@@ -0,0 +1,27 @@
+// console.log ("Naeem CodeHub");  comment clt+?
+
+// 1. Variable
+// 2. word vs keyword
+// 3. data type 
+
+// 1. Variable... 
+// code kay ander kisi data lo store krny k leye hm variable ka use krty hain 
+
+// let x1 ="Mehwish Hussain"
+
+// console.log(x1);
+
+// 2. Keyword... koi be word jis ka TS m koi matalb ho this is called keyword like Let
+// 2. Word... koi b esa word jis ka TS m koi mtlb na ho like boy girl women (word)
+// let x1 = "Mehwish Hussain"
+
+// 3.Data Type... koi b esa data jo text m ho us ko hm string kety hain 
+// let x1 = "Mehwish Hussain" x1 m error ay ga qk us ko string show krna hai hmy ts m 
+// let x : string = "Mehwish Hussain";
+let myName : string = "Mehwish Hussain" ;
+
+// Number... jo b data number m ho number type kehlata hai
+let age : number = 25
+
+// console.log(myName);
+console.log(age);
